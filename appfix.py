4a08f09f4bb8417ff15772af7823b163d236934a
SurAwall17 <surawalawal15@gmail.com>
2025-02-24 19:16:10 +0800

    first commit

--- appfix.py
+++ appfix.py
@@ -104,4 +104,5 @@ def rekomendasi():
     return jsonify(rekomendasi)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    # app.run(host='0.0.0.0', port=5000)
+    app.run();
